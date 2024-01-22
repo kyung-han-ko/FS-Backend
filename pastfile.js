b197de8677ad6a15ae5b3333b447c555dced9095
kyung-han-ko <rudgks0102@gmail.com>
2024-01-22 18:58:18 +0900

    2024-01-22

--- pastfile.js
+++ pastfile.js
@@ -12,6 +12,33 @@ const connection = mysql.createPool({  // mysql 접속 설정 // 크리에이트
 });
 
 // mysql과 연결 , 안되서 2로 시행함 됐음
+// const connection = mysql.createConnection(conn); // DB 커넥션 생성 , 함수로 돌려버리고 getConnection방식으로 진행
+// connection.getConnection(function(err,connection) {//커넥션추가
+//   if (err) throw err;
+//   console.log("Connected!");
+//   const sql = "INSERT INTO user_list (number ,email, name, password) VALUES ?";
+  
+//   const values = [
+//     ['1','rudgks0102@naver.com','고경한','2U4gjlyjUje1r2bK+Lg2umJozfH8pVkHnssN2nUzHEj1iJBvF3Wc8iLYq/LxErMndNaEPAJuOavMoBiCC/xvFg=='],
+//     ['2','rudgks0105@naver.com','박인수','2U4gjlyjUje1r2bK+Lg2umJozfH8pVkHnssN2nUzHEj1iJBvF3Wc8iLYq/LxErMndNaEPAJuOavMoBiCC/xvFg==']
+//   ];
+
+//   connection.query(sql, [values] , function (err, result) {
+//     if (err) throw err;
+//     console.log("Number of records inserted: " + result.affectedRows);
+//   });
+//   connection.release(); // 연결 해제 realease방식으로 바꿈. pool의 특성
+// });
+
+// connection.query('SELECT * FROM user_list', (error, rows, fields) => {
+// if (error){
+//   console.log('error : '+error)
+// }
+// else
+// console.log('result is: success ', rows);
+// });
+
+
 // 크립토
 const crypto = require("crypto");
 
@@ -116,116 +143,58 @@ app.listen(8080, function(){
         console.log("express Running on 8080.")
 });
 
-// 최종 가입하기 버튼 눌렀을 때 나오는 앱 포스트
+//최종 가입하기 버튼 눌렀을 때 나오는 앱 포스트
+
 app.post('/signup', (req, res) => {
   const email = req.body.email;
   const name = req.body.name;
-  const password1 = req.body.password1;
-  const password2 = req.body.password2;
+  const password1 = req.body.password1
+  const password2 = req.body.password2
   const emailNumber = req.body.emailNumber;
 
-  crypto.pbkdf2(
-      password1,
-      "secret", // 12월 4일 수정된 코드
-      100,
-      64,
-      'sha512',
+    crypto.pbkdf2(
+       password1,
+       "secret", //12월 4일 수정된 코드
+      100, 
+      64, 
+      'sha512', 
       (err, key) => {
-          console.log("암호화된 비밀번호 : ", key.toString('base64'));
-          connection.getConnection(function (err, connection) {
-              if (err) throw err;
-              console.log("crypto success");
+      console.log("암호화된 비밀번호 : ", key.toString('base64'));
+      connection.getConnection(function(err,connection) {//커넥션추가
+        if (err) throw err;
+        console.log("crypto success");
 
-              if (email && name && password1 && password2 && emailNumber) {
-                  connection.query('SELECT email FROM user_list WHERE email=?', email, function (err, emailRows) {
-                      if (err) throw err;
+        if (email && name && password1 && password2 && emailNumber) {
 
-                      if (Array.isArray(emailRows) && emailRows.length) {
-                          // 이메일 중복
-                          console.log('중복된 이메일이 있습니다');
-                          res.json({ success: false });
-                      } else {
-                          // 닉네임 중복 확인(UQ쓰려고 바꿈. 01-22수정)
-                          connection.query('SELECT name FROM user_list WHERE name=?', name, function (err, nameRows) {
-                              if (err) throw err;
-
-                              if (Array.isArray(nameRows) && nameRows.length) {
-                                  // 중복된 닉네임
-                                  console.log('중복된 닉네임이 있습니다');
-                                  res.json({ nickname: false });
-                              } else if (password1 !== password2) {
-                                  // 비번 확인
-                                  console.log('비밀번호와 비밀번호 확인이 일치하지 않습니다');
-                                  res.json({ test: false });
-                              } else {
-                                  // 다 맞으면 회원가입 성공
-                                  const sql = "INSERT INTO user_list (email, name, password) VALUES (?,?,?)";
-                                  const values = [email, name, key.toString('base64')];
-                                  connection.query(sql, values, function (err, result) {
-                                      if (err) throw err;
-                                      console.log("회원가입 성공")
-                                      console.log("DB추가된 인원 수: " + result.affectedRows + "명");
-                                      res.json({ success: true });
-                                  });
-                              }
-                          });
-                      }
-                  });
-              } else {
-                  // 하나라도 입력하지 않으면 경고창
-                  console.log("입력하지 않은 정보가 있습니다");
-                  res.json({ result: false });
-              }
-
-              connection.release();
-          });
-      });
+          connection.query('select email from user_list where email=?', email, function(err,rows){
+              if(Array.isArray(rows) && rows.length){
+                console.log('중복된 아이디가 있습니다');
+                res.json({success : false});
+              } else if(password1 != password2){
+                console.log('비밀번호와 비밀번호 확인이 일치하지 않습니다')
+                res.json({test : true})
+              } 
+              else {
+                const sql = "INSERT INTO user_list (email, name, password) VALUES (?,?,?)";
+                const values = [email,name,key.toString('base64')];
+                connection.query(sql, values , function (err, result) {
+                  if (err) throw err;
+                  console.log("DB추가된 인원 수: " + result.affectedRows + "명");
+                });
+                res.json({success : true})
+              } 
+            });
+  
+      } else {
+        console.log("입력하지 않은 정보가 있습니다");
+        res.json({result : true})
+      } 
+      connection.release(); // 연결 해제 realease방식으로 바꿈. pool의 특성
+    });
+}); 
+  
 });
 
-// 닉네임 중복검사
-app.post('/checkName', (req, res) => {
-  const nameCheck = req.body.name;
-
-  connection.getConnection(function (err, connection) {
-      if (err) throw err;
-
-      // 닉네임 중복 확인 쿼리
-      connection.query('SELECT name FROM user_list WHERE name=?', nameCheck, function (err, rows) {
-          if (err) throw err;
-
-          if (Array.isArray(rows) && rows.length) {
-              // 중복된 닉네임이 이미 존재하는 경우
-              res.json({ nickname: false});
-          } else {
-              // 중복되지 않은 경우
-              res.json({ nickname: true });
-          }
-      });
-
-      connection.release();
-  });
-});
-
-// 이메일 중복검사
-app.post('/emailCheck', (req, res) => {
-  const emailCheck = req.body.email;
-
-  connection.getConnection(function (err, connection) {
-      if (err) throw err;
-
-      connection.query('SELECT email FROM user_list WHERE email=?', emailCheck, function (err, rows) {
-          if (err) throw err;
-
-          if (Array.isArray(rows) && rows.length) {
-              res.json({ success: false });
-          } else {
-              res.json({ success: true });
-          }
-      });
-
-      connection.release();
-  });
-});
 //로그인 페이지
 
 const jwt = require('jsonwebtoken');
@@ -271,14 +240,13 @@ app.post('/login', (req, res) => {
 });
 
 // calendar part
-
 // insert
 app.post('/loadCalendar', (req, res) => {
   const data = req.body;
   console.log('넘어온 내용:', data);
 
-  const insertSql = 'INSERT INTO user_calendar (eventTitle, todayFood, kcalToday, fitToday, kcalFit, tomorrowFood, tomorrowFit, currentYM) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-  const values = [data.eventTitle, data.todayFood, data.kcalToday, data.fitToday, data.kcalFit, data.tomorrowFood, data.tomorrowFit, data.clientClickData];
+  const insertSql = 'INSERT INTO user_calendar (todayFood, kcalToday, fitToday, kcalFit, tomorrowFood, tomorrowFit, currentYM) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  const values = [data.todayFood, data.kcalToday, data.fitToday, data.kcalFit, data.tomorrowFood, data.tomorrowFit, data.clientClickData];
 
   connection.query(insertSql, values, (err, rows) => {
     if (err) {
@@ -309,23 +277,20 @@ app.get('/getCalendarData', (req, res) => {
   });
 });
 
-// delete
-app.post('/deleteCalendarData', (req, res) => {
-  const eventId = req.body.eventId;
-  console.log(eventId)
+// modal select
+// app.get('/getCalendar', (req, res) => {
+//   const selectSql = 'SELECT * FROM user_calendar WHERE DATE_FORMAT(currentYM, "%Y-%m-%d") = ?';
+//   const currentDate = req.query.currentYM;
 
-  const deleteSql = 'DELETE FROM user_calendar WHERE eventId = ?';
-  const values = [eventId];
-
-  connection.query(deleteSql, values, (err, result) => {
-    if (err) {
-      console.error('에러:', err);
-      res.json({ success: false, result: '데이터 삭제 실패' });
-    } else {
-      console.log('데이터 삭제 success');
-      res.json({ success: true, result: '데이터 삭제 success' });
-    }
-  });
-});
-
-/* */
+//   connection.query(selectSql, [currentDate], (err, rows) => {
+//     if (err) {
+//       console.error('에러:', err);
+//       res.json({ success: false, result: '데이터 조회 실패' });
+//     } else {
+//       console.log('데이터 조회 성공');
+//       console.log(rows);
+//       console.log(currentDate)
+//       res.json({ success: true, result: rows });
+//     }
+//   });
+// });
